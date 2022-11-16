d8b490d0eb9c99d4d21c0df2991a3c75e7de6892
李沙沙 <lishasha0601@icloud.com>
2022-11-16 20:20:14 +0800

    vue login

---
+++ libvue/src/views/login/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input type="text" v-model="unamw" placeholder="用户名"/>
+    <input type="text" v-model="upassword" placeholder="密码"/>
+    <button @click="login">登录</button>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+export default {
+  name: "login",
+  data () {
+    return {
+      uname:"",
+      upassword:"",
+      token: ''
+    };
+  },
+  methods: {
+    ...mapMutations(['changeLogin']),
+    login () {
+      let _this = this;
+      if (this.uname === '' || this.upassword === '') {
+        alert('账号或密码不能为空');
+      }
+          _this.userToken = 'Bearer ';
+          // 将用户token保存到vuex中
+          _this.changeLogin({ Authorization: _this.userToken });
+          _this.$router.push('/home');
+
+  }
+},
+}
+</script>
+
+<style scoped>
+
+</style>
