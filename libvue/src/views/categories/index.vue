3d2f6646497612c04e0a0da11c9bd7f4b7e9cdd5
杨高政 <3133983072@qq.com>
2022-11-23 10:37:54 +0800

    user index dialog

--- libvue/src/views/categories/index.vue
+++ libvue/src/views/categories/index.vue
@@ -1,5 +1,26 @@
 <template>
+  <template>
+    <el-button text @click="editVisible = true">
+      click to open the Dialog
+    </el-button>
 
+    <el-dialog
+      v-model="editVisible"
+      title="Tips"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <span>This is a message</span>
+      <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="editVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="editVisible = false">
+          Confirm
+        </el-button>
+      </span>
+      </template>
+    </el-dialog>
+  </template>
   <div class="common-layout">
     <el-container>
       <el-header style="padding: 20px">
@@ -11,10 +32,30 @@
               <el-table-column prop="uid" label="ID" />
               <el-table-column prop="username" label="Username" />
               <el-table-column label="Edit" >
-                <Edit @click="editUser" style="width: 1em; height: 1em; margin-right: 8px" />
-                <Delete @click="delUser" style="width: 1em; height: 1em; margin-right: 8px" />
+                <Edit @click="editVisible = true" style="width: 4em; height: 1em; margin-right: 8px" />
+<!--                <el-button type="default" @click="editVisible = true">查询</el-button>-->
+
+                <Delete @click="delUser" style="width: 4em; height: 1em; margin-right: 8px" />
               </el-table-column>
             </el-table>
+              <el-dialog v-model="editVisible" title="EditPage" width="55%" :before-close="handleClose">
+                <div class="common-layout">
+                  <el-container>
+<!--                    <el-header style="background-color: yellowgreen">Header</el-header>-->
+                    <el-container style="background-color: turquoise">
+                      <h1 style="font-size: 20px">Current Username</h1>
+                    </el-container>
+                  </el-container>
+                </div>
+                <template #footer>
+                <span class="dialog-footer">
+                  <el-button @click="editVisible = false">Cancel</el-button>
+                  <el-button type="primary" @click="editVisible = false">
+                    Confirm
+                  </el-button>
+                </span>
+                </template>
+              </el-dialog>
             <el-pagination background layout="prev, pager, next" :total="1000" style="text-align:center;"/>
           </div>
       </el-main>
@@ -27,14 +68,22 @@
 import { userList } from '@/api/user'
 // import { userEdit} from "@/api/user";
 import { ref } from 'vue'
+import { ElMessageBox } from 'element-plus'
+const editVisible = ref(false)
+const handleClose = (done) => {
+  ElMessageBox.confirm('Are you sure to close this dialog?')
+    .then(() => {
+      done()
+    })
+    .catch(() => {
+      // catch error
+    })
+}
+
 const user = ref([])
 const userInit = async () => {
   user.value = await userList()
 }
 userInit()
-// const editUser = async () => {
-//   user.value = await userEdit()
-// }
-
 </script>
 <style lang="scss" scoped></style>
