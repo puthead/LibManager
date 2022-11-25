dbb72b568cda58e29fed0d13218eccf9ffc728dd
杨高政 <3133983072@qq.com>
2022-11-25 10:43:48 +0800

    前端传参

--- libvue/src/views/categories/index.vue
+++ libvue/src/views/categories/index.vue
@@ -28,17 +28,17 @@
       </el-header>
       <el-main>
           <div style="border: 2px solid rgb(205, 202, 209);border-radius: 15px;padding:10px 40px; ">
-              <table>
-                <thead><th>ID</th><th>UserName</th><th>EDIT</th></thead>
+              <table style="width: 100%">
+                <thead ><th>ID</th><th>UserName</th><th>EDIT</th></thead>
                 <tbody>
-                <tr v-for="item in user" :key="item.uid"><td>{{item.uid}}</td><td>{{item.username}}</td><Edit @click="editVisible = true;editSubmit('{{item.uid}}','{{item.username}}')" style="width: 4em; height: 1em; margin-right: 8px" /></tr>
+                <tr v-for="item in user" :key="item.uid" style="text-align: center"><td>{{item.uid}}</td><td>{{item.username}}</td><td><Edit @click="editVisible = true;editSubmit(item.uid,item.username)" style="width: 4em; height: 1em; margin-right: 8px" /></td></tr>
                 </tbody>
               </table>
               <el-dialog v-model="editVisible" title="EditPage" width="55%" :before-close="handleClose">
                 <div class="common-layout">
                   <el-container style="padding-left: 80px;margin-bottom: 40px">
                         <h1 style="font-size: 20px;margin-top: 3px">Current Username: </h1>
-                        <el-input v-model="Username" disabled placeholder="Please input" prop="username" style="left:20px ;width: 200px"/>
+                        <el-input v-model="Username" disabled placeholder="{{name}}" prop="name" style="left:20px ;width: 200px"/>
                   </el-container>
                   <el-container style="padding-left: 80px;margin-bottom: 40px">
                         <h1 style="font-size: 20px;margin-top: 3px">New Username: </h1>
@@ -72,6 +72,8 @@ import { userList } from '@/api/user'
 import { ref } from 'vue'
 import { ElMessageBox } from 'element-plus'
 
+var id = 0
+var name = ''
 const editVisible = ref(false)
 const handleClose = (done) => {
   ElMessageBox.confirm('真的要关闭此窗口吗？')
@@ -85,15 +87,14 @@ const handleClose = (done) => {
 
 function editSubmit(uid, username) {
   console.log(uid, username)
+   id = uid
+   name = username
+  console.log('id=' + id + '; name=' + name)
 }
 const Username = ref('')
 const newUsername = ref('')
 const newPassword = ref('')
-// const data = ref({
-//   newUsername: '',
-//   newPassword: '',
-//   uid: 7
-// })
+
 const user = ref([])
 const userInit = async () => {
   user.value = await userList()
